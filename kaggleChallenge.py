4f5a25fdd84b1de9a1d077026d011d5690883d01
Akshita Raina <akshitar@usc.edu>
2016-02-29 23:54:11 -0800

    no message

--- kaggleChallenge.py
+++ kaggleChallenge.py
@@ -6,10 +6,12 @@ import pandas as pd
 from sklearn import cross_validation
 from sklearn import svm
 from patsy import dmatrix
-from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
 from sklearn.feature_selection import SelectKBest, f_classif, chi2
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.cross_validation import KFold,train_test_split
+
+np.set_printoptions(precision=5, threshold=np.inf)
 
 # Function for evaluating new set of features for different thresholds of chi2 values
 def chi2Dataframe(threshold,dataset,chiValues):
@@ -20,15 +22,12 @@ def chi2Dataframe(threshold,dataset,chiValues):
         else: continue
     return newDataset
 
-np.set_printoptions(precision=5, threshold=np.inf)
-
 # Read Training data CSV file directly from the desktop and save the results
 result = pd.read_csv("/home/ubuntu/walmartdata(woV).csv").dropna(axis  = 1, how = 'any')
 
 #Giving asymmetrical class numbers symmetry
-result['TripType'] = pd.to_numeric(result['TripType'])
+result['TripType'] = result['TripType'].convert_objects(convert_numeric=True)
 clsNum = np.sort(pd.unique(result['TripType'].ravel()))
-
 j = 1
 for i in clsNum:
     result['TripType'].loc[result['TripType']==i] = j
@@ -47,15 +46,22 @@ print("The number of samples= {0}\nThe number of features= {1}".format(rows,cols
 feature_number = list(range(1,cols))
 dataset = dataset.replace(' ', 0)
 for i in range(0, cols):
-    dataset[i] = pd.to_numeric(dataset[i])
+    dataset[i] = dataset[i].convert_objects(convert_numeric=True)
 
 # Caluculate the chi2 values
 chiValues,pval = chi2(dataset[feature_number],dataset[0])
 threshold = 300
 newDataset = chi2Dataframe(threshold, dataset,chiValues)
 
-Cs = np.linspace(-5, 15, num=11, dtype=float)
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(newDataset, dataset[0], test_size=0.3,random_state=1)
-for i in range(0,len(Cs)):
-    clf = svm.SVC(kernel='linear', C=(2**Cs[i])).fit(X_train, y_train)
-    print clf.score(X_test, y_test)
+zeroCol = pd.DataFrame(dataset[0])
+newDataset = pd.concat([zeroCol , newDataset] , join='outer', axis =1)
+newDataset.columns = arange(0,len(newDataset.columns))
+split = np.random.permutation(int(rows*0.5))
+redDataset = newDataset.iloc[split]
+rows,cols = redDataset.shape
+feature_number = list(range(1,cols))
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(redDataset[feature_number], redDataset[0], test_size=0.3,random_state=1)
+
+clf = svm.LinearSVC(C=10,penalty='l1', dual=False, loss='squared_hinge').fit(X_train, y_train)
+print("Score method for validation with L1: {0}".format(clf.score(X_test, y_test)))
+print("Score method for training with L1: {0}".format(clf.score(X_train, y_train)))
